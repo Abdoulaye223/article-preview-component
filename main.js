4bfd98e07432ae9f2d5094c52723e35b8a94ef18
Abdoulaye223 <156548566+Abdoulaye223@users.noreply.github.com>
2024-07-20 03:13:02 +0000

    Done

---
+++ main.js
@@ -0,0 +1,8 @@
+const shareOption = document.querySelector('.share-option');
+
+document.querySelector('.share-button').addEventListener('click', 
+    function () {
+        this.classList.toggle('active');
+        shareOption.classList.toggle('active');
+    }
+);
